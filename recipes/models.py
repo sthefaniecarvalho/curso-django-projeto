4496f1d4eaffb0e85f7e4ff33d6305d5dd3be08d
Sthefanie <sthefaniegc06@gmail.com>
2023-04-25 15:36:19 -0300

    Criando models  e aplicando migration

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+
+class Recipes(models.Model):
+    title = models.CharField(max_length=65)
+    desciption = models.CharField(max_length=65)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_step = models.TextField()
+    preparation_step_is_html = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)  # set_null -> SE CASO NAO EXISTIR MAIS A CATEGORY OS VALORES FICARAM NULL
