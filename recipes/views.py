b702436a34aa02ad2dd952d172710ecc2b381b2b
Sthefanie <sthefaniegc06@gmail.com>
2023-05-05 16:00:01 -0300

    Usando o shortcut get_list_or_404 (atalho)

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import get_list_or_404, render
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 
@@ -12,15 +11,13 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
+    recipes = get_list_or_404(Recipe.objects.filter(
         category__id=category_id, is_published=True).order_by('-id')
-
-    if not recipes:
-        raise Http404('Not found')
+    )
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes.first().category.name} - Category',
+        'title': f'{recipes[0].category.name} - Category',
     })
 
 
