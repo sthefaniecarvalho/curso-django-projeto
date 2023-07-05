815dbfe4bd3b83f2499a3092e1edfea6e202a82a
Sthefanie <sthefaniegc06@gmail.com>
2023-07-05 14:21:56 -0300

    Validando max_length

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,4 +1,5 @@
 from .test_recipe_base import RecipeTestBase
+from django.core.exceptions import ValidationError
 
 
 class RecipeModelTest(RecipeTestBase):
@@ -6,6 +7,8 @@ class RecipeModelTest(RecipeTestBase):
         self.recipe = self.make_recipe()
         return super().setUp()
 
-    def test_the_test(self):
-        recipe = self.recipe
-        ...
+    def test_recipe_title_raises_error_if_title_has_more_than_65_chars(self):
+        self.recipe.title = 'A' * 70
+
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
